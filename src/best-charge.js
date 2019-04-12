9f0feb0c2f8fbfa93d5f9c6069b1952b66683b2a
NICE-FUTURE <2375558167@qq.com>
2019-04-12 18:06:25 +0800

    update code according to tasking file

--- src/best-charge.js
+++ src/best-charge.js
@@ -1,45 +1,63 @@
 // var loadAllPromotions = require('./promotions.js');  // 加载优惠信息
 // var loadAllItems = require('./items.js');  // 加载菜品项
 
-function halfPrice(order, tar) {
-    //指定菜品半价
-    var prom = 0;
-    var names = [];
-    order.forEach(function(item) {
-        if (tar.indexOf(item.id) != -1) {
-            prom += item.price/2;
-            names.push(item.name);
-        }
-    })
-    return {'note': '指定菜品半价('+names.join('，')+')', 'prom': prom};
-}
+function bestCharge(selectedItems) {
 
-function specialPromotion(order) {
-    //满减优惠
-    var total = 0;
-    order.forEach(function(item) {
-        total += item.price*item.count;
-    })
-    return {'note': '满30减6元', 'prom': Math.floor(total/30)*6};
-}
+    var items = loadAllItems();
+    var scale = loadPromotions()[1].items;
 
-function id2item(items, id) {
-    // 将 id 对应到 item 返回
-    for (let item of items) {
-        if (item.id === id) return item; 
-    }
-    return null;
+    var order = generateOrder(selectedItems, items);  // 转换成订单
+    var promotion = calculatePromotion(order, scale);  // 计算优惠
+    var summary = generateSummary(order, promotion);  // 生成描述
+
+    return summary;
 }
 
 function generateOrder(selectedItems, items) {
     // 生成订单信息
     order = [];
-    selectedItems.forEach(function(ele){
+    for (let ele of selectedItems) {
         pieces = ele.split(' x ');
-        item = id2item(items, pieces[0]);
-        order.push({'id': pieces[0], 'count': +pieces[1], 'name': item.name, 'price': item.price});
-    })
+        for (let item of items) {
+            if (item.id === pieces[0]) {
+                order.push({'id': pieces[0], 'count': +pieces[1], 'name': item.name, 'price': item.price});
+            }
+        }
+    }
     return order;
+}
+
+function calculatePromotion(order, scale) {
+    //指定菜品半价，scale 中的 id 进行半价优惠
+    var prom = 0;
+    var names = [];
+    order.forEach(function(item) {
+        if (scale.indexOf(item.id) != -1) {
+            prom += item.price/2;
+            names.push(item.name);
+        }
+    })
+    var prom1 = {
+        'note': '指定菜品半价('+names.join('，')+')',
+        'prom': prom
+    };
+
+    //满减优惠
+    var total = 0;
+    order.forEach(function(item) {
+        total += item.price*item.count;
+    })
+    var prom2 = {
+        'note': '满30减6元',
+        'prom': Math.floor(total/30)*6
+    };
+
+    //比较两种优惠
+    if (prom1.prom > prom2.prom) {
+        return prom1;
+    } else {
+        return prom2;
+    }
 }
 
 function generateSummary(order, prom) {
@@ -61,35 +79,11 @@ function generateSummary(order, prom) {
     return summary;
 }
 
-function bestCharge(selectedItems) {
-
-    var items = loadAllItems();
-    var promotions = loadPromotions();
-
-    var order = generateOrder(selectedItems, items);  // 转换成订单
-    
-    var prom1 = halfPrice(order, promotions[1].items);  // 检查指定菜品半价优惠
-    var prom2 = specialPromotion(order);  // 检查满减优惠
-
-    // 比较优惠力度，判断并输出
-    var prom;
-    if (prom1.prom > prom2.prom) {
-        prom = prom1;
-    } else {
-        prom = prom2;
-    }
-
-    // 生成描述
-    var summary = generateSummary(order, prom);
-
-    return summary;
-}
-
-let inputs = ["ITEM0001 x 1", "ITEM0013 x 2", "ITEM0022 x 1"];
+//let inputs = ["ITEM0001 x 1", "ITEM0013 x 2", "ITEM0022 x 1"];
 // let inputs = ["ITEM0013 x 4", "ITEM0022 x 1"];
 // let inputs = ["ITEM0013 x 4"];
-let result = bestCharge(inputs).trim();
-console.log(result);
+//let result = bestCharge(inputs).trim();
+//console.log(result);
 // for (i of result) {
 //     console.log(i);
 // }
